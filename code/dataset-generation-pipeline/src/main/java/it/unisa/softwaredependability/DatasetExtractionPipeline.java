e649cd98eaa1ad29c57e8ebdb535f119e25ba5fd
Martin Steinhauer <martin.steinhauer@sulkyloops.com>
2020-04-13 22:07:18 +0200

    Add group by project language and count Add command line parser

--- code/dataset-generation-pipeline/src/main/java/it/unisa/softwaredependability/DatasetExtractionPipeline.java
+++ code/dataset-generation-pipeline/src/main/java/it/unisa/softwaredependability/DatasetExtractionPipeline.java
@@ -1,18 +1,13 @@
 package it.unisa.softwaredependability;
 
 import it.unisa.softwaredependability.config.DatasetHeader;
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-import scala.Tuple2;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Pattern;
+import static org.apache.spark.sql.functions.count;
+
 
 
 public class DatasetExtractionPipeline {
@@ -33,13 +28,16 @@ public class DatasetExtractionPipeline {
                 .option("header", "false")
                 .option("mode", "DROPMALFORMED")
                 .schema(DatasetHeader.getProjectHeader())
-                .load(sourceFile)
-                .select("*")
-                .where("language = 'Java'");
+                .load(sourceFile);
 
-        System.out.println("Found projects in Java: "+dataset.count());
+        JavaRDD<Row> results = dataset
+                .filter("id != -1")
+                .select("language")
+                .groupBy("language")
+                .count()
+                .toJavaRDD();
 
-
+        results.saveAsTextFile("/Users/martinsteinhauer/Desktop/results.txt");
 
         session.close();
     }
--- code/dataset-generation-pipeline/src/main/java/it/unisa/softwaredependability/Main.java
+++ code/dataset-generation-pipeline/src/main/java/it/unisa/softwaredependability/Main.java
@@ -5,6 +5,11 @@ import org.apache.spark.SparkConf;
 public class Main {
 
     public static void main(String[] args) {
-        DatasetExtractionPipeline pipeline = new DatasetExtractionPipeline("/Volumes/Elements/github\\ archive/github-2019-06-01/projects.csv");
+        for(String arg : args) {
+            System.out.println(arg);
+        }
+        String sourceFile = "/Volumes/Elements/github archive/samples/spring-projects.csv";
+        sourceFile = "/Volumes/Elements/github\\ archive/github-2019-06-01/projects.csv";
+        DatasetExtractionPipeline pipeline = new DatasetExtractionPipeline(sourceFile);
     }
 }
