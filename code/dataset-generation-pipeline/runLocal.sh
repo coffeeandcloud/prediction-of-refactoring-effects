62e3cc5d7ff57b8f7013c92a44643b576840d0ab
Martin <martin.steinhauer@st.oth-regensburg.de>
2020-06-12 18:19:28 +0200

    Add shell scripts for simpler execution

---
+++ code/dataset-generation-pipeline/runLocal.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+rm -R /Users/martinsteinhauer/Desktop/commits
+rm -R /Users/martinsteinhauer/Desktop/commitmetricresults
+
+~/sdk/spark/bin/spark-submit --master "local[1]" \
+--class it.unisa.softwaredependability.Main \
+--driver-memory 8g \
+--conf "spark.serializer=org.apache.spark.serializer.KryoSerializer" \
+target/dataset-generation-pipeline-1.0-SNAPSHOT.jar \
+--input "datasets/small.csv" \
+--commits-output "/Users/martinsteinhauer/Desktop/commits" \
+--metrics-output "/Users/martinsteinhauer/Desktop/commitmetricresults" \
+--refactoring-mining-only \
+--parallel-jobs 1 \
+--parallel-repos 1 \
+--batch-size 100 \
+--username "<yourGithubUsername>" \
+--token "<yourGithubAccessToken>"
